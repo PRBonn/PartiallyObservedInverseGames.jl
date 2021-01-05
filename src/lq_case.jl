88884bc309d08792ed07e093d0af430141cacc0a
lassepe <lasse.peters@mailbox.org>
2021-01-05 14:59:38 +0100

    lq_case: compute time index from axis(x)[2]

--- src/lq_case.jl
+++ src/lq_case.jl
@@ -22,8 +22,8 @@ x0 = [10.0, 10.0]
 
 #============================================== Utils ==============================================#
 
-function dynamics_constraints(x, u; A, B, T)
-    reduce(hcat, ((x[:, t + 1] - A[t] * x[:, t] - B[t] * u[:, t]) for t in 1:(T - 1)))
+function dynamics_constraints(x, u; A, B)
+    reduce(hcat, ((x[:, t + 1] - A[t] * x[:, t] - B[t] * u[:, t]) for t in axes(x)[2][1:(end - 1)]))
 end
 
 #=========================================== Forward LQR ===========================================#
@@ -34,7 +34,7 @@ function solve_lqr(A, B, Q, R, x0, T)
     model = JuMP.Model(Ipopt.Optimizer)
     @variable(model, x[1:n_states, 1:T])
     @variable(model, u[1:n_controls, 1:T])
-    @constraint(model, dynamics, dynamics_constraints(x, u; A, B, T) .== 0)
+    @constraint(model, dynamics, dynamics_constraints(x, u; A, B) .== 0)
     @constraint(model, initial_condition, x[:, 1] .== x0)
     @objective(model, Min, forward_objective(x, u; Q, R))
     JuMP.optimize!(model)
@@ -46,25 +46,29 @@ lqr_solution, _ = solve_lqr(A, B, Q, R, x0, T)
 #=========================================== Inverse LQR ===========================================#
 
 "The lagrangian of the forward LQR problem."
-function lqr_lagrangian(x, u, λ; Q, R, A, B, T)
-    c_dyn = dynamics_constraints(x, u; A, B, T)
-    @views forward_objective(x, u; Q, R) + sum(λ[:, t]' * c_dyn[:, t] for t in 1:(T - 1))
+function lqr_lagrangian(x, u, λ; Q, R, A, B)
+    c_dyn = dynamics_constraints(x, u; A, B)
+    @views forward_objective(x, u; Q, R) +
+           sum(λ[:, t]' * c_dyn[:, t] for t in axes(x)[2][1:(end - 1)])
 end
 
 "The hand-written gradient of the forward LQR problem in x."
-function lqr_lagrangian_grad_x(x, u, λ; Q, R, A, B, T)
+function lqr_lagrangian_grad_x(x, u, λ; Q, R, A, B)
     hcat(
         2 * Q * x[:, 1] - A[1]' * λ[:, 1], # special handling of first time step
         reduce(
             hcat,
-            (2 * Q * x[:, t + 1] + λ[:, t] - (λ[:, t + 1]' * A[t + 1])' for t in 1:(T - 1)),
+            (
+                2 * Q * x[:, t + 1] + λ[:, t] - (λ[:, t + 1]' * A[t + 1])'
+                for t in axes(x)[2][1:(end - 1)]
+            ),
         ),
     )
 end
 
 "The hand-written gradient of the forward LQR problem in u."
-function lqr_lagrangian_grad_u(x, u, λ; Q, R, A, B, T)
-    reduce(hcat, (2 * R * u[:, t] - (λ[:, t]' * B[t])' for t in 1:T))
+function lqr_lagrangian_grad_u(x, u, λ; Q, R, A, B)
+    reduce(hcat, (2 * R * u[:, t] - (λ[:, t]' * B[t])' for t in axes(x)[2]))
 end
 
 """
@@ -90,12 +94,12 @@ function solve_inverse_lqr(x̂, Q̃, R̃; A, B, r_sqr_min = 1e-5)
     # initial condition
     @constraint(model, initial_condition, x[:, 1] .== x̂[:, 1])
     # dynamics
-    @constraint(model, dynamics_constraints(x, u; A, B, T) .== 0)
+    @constraint(model, dynamics_constraints(x, u; A, B) .== 0)
     # Optimality conditions (KKT) of forward LQR show up as a constraints
     Q = sum(q .* Q̃)
     R = sum(r .* R̃)
-    @constraint(model, ∇ₓL, lqr_lagrangian_grad_x(x, u, λ; Q, R, A, B, T)[:, 2:end] .== 0)
-    @constraint(model, ∇ᵤL, lqr_lagrangian_grad_u(x, u, λ; Q, R, A, B, T) .== 0)
+    @constraint(model, ∇ₓL, lqr_lagrangian_grad_x(x, u, λ; Q, R, A, B)[:, 2:end] .== 0)
+    @constraint(model, ∇ᵤL, lqr_lagrangian_grad_u(x, u, λ; Q, R, A, B) .== 0)
     # regularization
     @constraint(model, r' * r >= r_sqr_min)
     @constraint(model, r' * r + q' * q == 1)
@@ -125,7 +129,7 @@ end
 
     @testset "Gradient Check" begin
         grad_args = (ilqr_solution.x, ilqr_solution.u, ilqr_solution.λ)
-        grad_kwargs = (; Q = Q_est, R = R_est, A, B, T)
+        grad_kwargs = (; Q = Q_est, R = R_est, A, B)
 
         ∇ₓL_ad, ∇ᵤL_ad = Zygote.gradient(
             (x, u) -> lqr_lagrangian(x, u, ilqr_solution.λ; grad_kwargs...),
