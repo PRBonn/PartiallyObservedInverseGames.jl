a4bb447b7777f5306f0f03b4cb29cbf4c2fac138
lassepe <lasse.peters@mailbox.org>
2021-01-31 16:50:05 +0100

    Add tests for collion avoidance forward and inverse game

--- test/collision_avoidance_game.jl
+++ test/collision_avoidance_game.jl
@@ -2,7 +2,8 @@ using Test: @test, @testset
 
 using JuMP: @objective
 using JuMPOptimalControl.DynamicsModelInterface: visualize_trajectory
-using JuMPOptimalControl.ForwardGame: IBRGameSolver, solve_game
+using JuMPOptimalControl.ForwardGame: IBRGameSolver, KKTGameSolver, solve_game
+using JuMPOptimalControl.InverseGames: InverseIBRSolver, InverseKKTSolver, solve_inverse_game
 
 unique!(push!(LOAD_PATH, joinpath(@__DIR__, "utils")))
 import TestUtils
@@ -25,7 +26,7 @@ function generate_player_cost_model(;
     state_indices,
     input_indices,
     goal_position,
-    weights = (; state_velocity = 10, state_goal = 0.1, control_Δv = 100, control_Δθ = 10),
+    weights = (; state_goal = 0.1, state_velocity = 10, control_Δv = 100, control_Δθ = 10),
 )
 
     function objective(x, u; weights)
@@ -35,25 +36,35 @@ function generate_player_cost_model(;
         @views x_sub = x[state_indices, :]
         @views u_sub = u[input_indices, :]
 
-        weights[:state_velocity] * sum(el -> el^2, x_sub[3, :]) +
         weights[:state_goal] * sum(el -> el^2, x_sub[1:2, :] .- goal_position) +
+        weights[:state_velocity] * sum(el -> el^2, x_sub[3, :]) +
         weights[:control_Δv] * sum(el -> el^2, u_sub[1, :]) +
         weights[:control_Δθ] * sum(el -> el^2, u_sub[2, :])
     end
 
     function objective_gradients(x, u; weights)
-        # TODO: This typic probably does not allow to assign expressioins later. Make a non-inplace
-        # version or widen the type of dJdx, dJdu.
-        dJdx = zeros(size(x))
-        dJdu = zeros(size(u))
+        n_states, T = size(x)
+        n_controls = size(u, 1)
         @views x_sub = x[state_indices, :]
         @views u_sub = u[input_indices, :]
-        @views dJdx_sub = dJdx[state_indices, :]
-        @views dJdu_sub = dJdu[input_indices, :]
-        dJdx_sub[3, :] .+= 2 * weights[:state_velocity] * x_sub[3, :]
-        dJdx_sub[1:2, :] .+= 2 * weights[:state_goal] * (x_sub[1:2, :] .- goal_position)
-        dJdu_sub[1, :] .+= 2 * weights[:control_Δv] * u_sub[1, :]
-        dJdu_sub[2, :] .+= 2 * weights[:control_Δθ] * u_sub[2, :]
+
+        dJdx_sub = [
+            2 * weights[:state_goal] * (x_sub[1:2, :] .- goal_position)
+            2 * weights[:state_velocity] * x_sub[3, :]'
+            zeros(1, T)
+        ]
+        dJdu_sub = 2 .* [weights[:control_Δv], weights[:control_Δθ]] .* u_sub
+
+        dJdx = [
+            zeros(first(state_indices) - 1, T)
+            dJdx_sub
+            zeros(n_states - last(state_indices), T)
+        ]
+        dJdu = [
+            zeros(first(input_indices) - 1, T)
+            dJdu_sub
+            zeros(n_controls - last(input_indices), T)
+        ]
 
         (; dx = dJdx, du = dJdu)
     end
@@ -85,14 +96,49 @@ player_cost_models = let
     (cost_model_p1, cost_model_p2)
 end
 
-x0 = vcat([-1, 0, 0, 0], [0, -1, 0, pi / 2])
+observation_model = (; σ = 0, expected_observation = identity)
+x0 = vcat([-1, 0, 0, 0 + deg2rad(10)], [0, -1, 0, pi / 2 + deg2rad(10)])
 T = 100
 
-ibr_converged, ibr_solution, ibr_models =
-    solve_game(IBRGameSolver(), control_system, player_cost_models, x0, T)
-
 @testset "Forward Game" begin
-    @test ibr_converged
+    @testset "IBR" begin
+        global ibr_converged, ibr_solution, ibr_models =
+            solve_game(IBRGameSolver(), control_system, player_cost_models, x0, T)
+
+        @test ibr_converged
+    end
+
+    @testset "KKT" begin
+        global kkt_solution, kkt_model = solve_game(
+            KKTGameSolver(),
+            control_system,
+            player_cost_models,
+            x0,
+            T;
+            init = ibr_solution,
+        )
+    end
 end
 
-visualize_trajectory(control_system, ibr_solution.x)
+@testset "Inverse Game" begin
+    @testset "KKT" begin
+        global inverse_kkt_solution, inverse_kkt_model = solve_inverse_game(
+            InverseKKTSolver(),
+            ibr_solution.x;
+            control_system,
+            player_cost_models,
+            init = kkt_solution,
+        )
+
+        for (cost_model, weights) in zip(player_cost_models, inverse_kkt_solution.player_weights)
+            TestUtils.test_inverse_solution(weights, cost_model.weights)
+        end
+
+        TestUtils.test_inverse_model(
+            inverse_kkt_model,
+            observation_model,
+            ibr_solution.x,
+            ibr_solution.x,
+        )
+    end
+end
