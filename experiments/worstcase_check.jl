fd97e5d1aa23c6409d216eee46d53756ea25ee8c
lassepe <lasse.peters@mailbox.org>
2021-02-26 11:53:20 +0100

    Rename: highway_sanity_check.jl worstcase_check.jl

--- experiments/worstcase_check.jl
+++ experiments/worstcase_check.jl
@@ -1,18 +1,19 @@
 using Query: @groupby, @map, @join, key
 using DataFrames: DataFrame
 
-highway_worstcases =
+worstcases =
     errstats |>
     @groupby(_.estimator_name) |>
     @map({
         estimator_name = key(_),
         worst_case_observation_idx =
             getindex(_, argmax(_.position_estimation_error)).observation_idx,
+        convergence_failures = count(!e.converged for e in _),
     }) |>
     @join(
         estimates,
         Any[_.worst_case_observation_idx, _.estimator_name],
         Any[_.observation_idx, _.estimator_name],
-        {_.estimator_name, _.worst_case_observation_idx, __.x}
+        {_..., worst_x = __.x}
     ) |>
     DataFrame
