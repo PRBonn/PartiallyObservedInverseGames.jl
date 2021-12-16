09879e1dbca3a786bb00d6a99fe1d11a32cc1159
lassepe <lasse.peters@mailbox.org>
2021-12-16 12:02:41 +0100

    Port receding horizon experiment to highway settting

--- experiments/receding_horizon.jl
+++ experiments/receding_horizon.jl
@@ -8,11 +8,58 @@ using GLMakie: GLMakie
 using Makie: Makie
 
 include("utils/preamble.jl")
-include("utils/unicycle_online_setup.jl")
+load_cache_if_not_defined!("receding_horizon")
 
-function visualize_receding_horizon(sim_result, domain = 1.5)
-    fig = Makie.Figure()
-    ax = Makie.Axis(fig[1, 1]; aspect = 1, limits = ((-domain, domain), (-domain, domain)))
+function setup_highway(player_configurations; Δt = 1.0, T)
+    x0 = mapreduce(vcat, player_configurations) do player_config
+        [
+            player_config.initial_lane,
+            player_config.initial_progress,
+            player_config.initial_speed,
+            deg2rad(90),
+        ]
+    end
+
+    control_system = TestDynamics.ProductSystem([
+        TestDynamics.Unicycle(Δt),
+        TestDynamics.Unicycle(Δt),
+        TestDynamics.Unicycle(Δt),
+        TestDynamics.Unicycle(Δt),
+        TestDynamics.Unicycle(Δt),
+    ])
+
+    position_indices = mapreduce(vcat, eachindex(control_system.subsystems)) do subsystem_idx
+        TestDynamics.state_indices(control_system, subsystem_idx)[1:2]
+    end
+
+    partial_state_indices = mapreduce(vcat, eachindex(control_system.subsystems)) do subsystem_idx
+        TestDynamics.state_indices(control_system, subsystem_idx)[[1, 2, 4]]
+    end
+
+    player_cost_models_gt = map(Iterators.countfrom(1), player_configurations) do ii, player_config
+        cost_model_p1 = CollisionAvoidanceGame.generate_player_cost_model(;
+            player_idx = ii,
+            control_system,
+            T,
+            goal_position = nothing,
+            weights = (;
+                state_proximity = player_config.prox_cost,
+                state_velocity = player_config.speed_cost,
+                control_Δv = 1,
+                control_Δθ = 1,
+            ),
+            fix_costs = (; state_goal = 100, state_lane = 0.1, state_orientation = 2.0),
+            y_lane_center = player_config.target_lane,
+            target_speed = player_config.target_speed,
+        )
+    end
+
+    (; T, x0, control_system, position_indices, partial_state_indices, player_cost_models_gt)
+end
+
+function visualize_receding_horizon_interactive(sim_result)
+    fig = Makie.Figure(; resolution = (600, 300))
+    ax = Makie.Axis(fig[1, 1], limits = ((-5, 32), (-2, 2)))
 
     time_slider = Makie.Slider(fig[2, 1]; range = eachindex(sim_result.estimator_steps))
     step = Makie.@lift sim_result.estimator_steps[$(time_slider.value)]
@@ -26,40 +73,45 @@ function visualize_receding_horizon(sim_result, domain = 1.5)
             Makie.@lift [Makie.Point2f(y_full[p]) for y_full in eachcol($step.y_full)]
 
         # line and point for ground truth
-        Makie.lines!(ax, ground_truth; color = "green")
-        Makie.scatter!(ax, Makie.@lift ground_truth[$step.t]; color = "green")
+        Makie.lines!(ax, ground_truth; color = "gray")
+        Makie.scatter!(ax, Makie.@lift ground_truth[$step.t]; color = "black")
 
         # line and point for prediction
-        Makie.lines!(ax, prediction; color = "blue")
-        Makie.scatter!(ax, Makie.@lift $prediction[$buffer_size]; color = "blue")
+        Makie.lines!(ax, prediction; color = "green")
+        Makie.scatter!(ax, Makie.@lift $prediction[$buffer_size]; color = "green")
 
         # raw observations in the buffer
         Makie.scatter!(ax, observations_full; color = ("gray", 0.5))
     end
 
+    ax.aspect = Makie.DataAspect()
+
+    Makie.rotate!(ax.scene, -pi / 2)
+
     fig
 end
 
-function simulate(;
-    T_gt = 50,
+function simulate(
+    setup;
     T_predict = 10,
-    T_obs = 10,
-    σ = 0.01,
+    T_obs = 5,
+    σ = 0.05,
     t_start = 5,
     u_regularization = 1e-5,
+    prior_weight = 1e-2,
     use_heuristic_prior = true,
 )
 
     # Generate a longer demo
     rng = Random.MersenneTwister(1)
-    observation_model = (; σ, expected_observation = x -> x[partial_state_indices, :])
+    observation_model = (; σ, expected_observation = x -> x[setup.partial_state_indices, :])
 
     converged_gt, forward_solution_gt = ForwardGame.solve_game(
         IBRGameSolver(),
-        control_system,
-        player_cost_models_gt,
-        x0,
-        T_gt;
+        setup.control_system,
+        setup.player_cost_models_gt,
+        setup.x0,
+        setup.T;
         solver_attributes = (; print_level = 1),
     )
 
@@ -70,7 +122,7 @@ function simulate(;
     estimator_steps = NamedTuple[]
     init = nothing
     previous_solution = nothing
-    ProgressMeter.@showprogress for t in t_start:(T_gt - T_predict)
+    ProgressMeter.@showprogress for t in t_start:(setup.T - T_predict)
         obs_window = max((t - T_obs + 1), 1):t
         @assert length(obs_window) <= T_obs
         y = observations_partial[:, obs_window]
@@ -81,7 +133,7 @@ function simulate(;
                 presolve_converged, presolve_solution = InversePreSolve.pre_solve(
                     y,
                     nothing;
-                    control_system,
+                    setup.control_system,
                     observation_model,
                     #observation_model = (; expected_observation = identity),
                     T = length(obs_window) + T_predict,
@@ -107,14 +159,15 @@ function simulate(;
         converged, solution = solve_inverse_game(
             InverseKKTConstraintSolver(),
             y;
-            control_system,
+            setup.control_system,
             observation_model,
-            player_cost_models = player_cost_models_gt,
+            player_cost_models = setup.player_cost_models_gt,
             T_predict,
             solver_attributes = (; print_level = 1),
             cmin = 1.e-3,
             pre_solve = true,
             prior,
+            prior_weight,
             init_with_observation = false,
             pre_solve_kwargs = (; u_regularization),
             init,
@@ -129,7 +182,67 @@ function simulate(;
     (;
         estimator_steps,
         forward_solution_gt,
-        position_indices = position_indices |> ii -> Iterators.partition(ii, 2) |> collect,
+        position_indices = setup.position_indices |> ii -> Iterators.partition(ii, 2) |> collect,
     )
 end
+
+function main(player_configurations = player_configurations; T_gt = 60, sim_kwargs = (;))
+    setup = setup_highway(player_configurations; T = T_gt)
+    @run_cached sim_result = RecedingHorizon.simulate(setup; sim_kwargs...)
+    visualize_receding_horizon_interactive(sim_result)
+end
+
+player_configurations = [
+    # Vehicle on the right lane wishing to merge left to go faster
+    (;
+        initial_lane = 1.0,
+        initial_progress = -2,
+        initial_speed = 0.2,
+        target_speed = 0.4,
+        speed_cost = 1.5,
+        target_lane = -1.0,
+        prox_cost = 0.3,
+    ),
+    # Fast vehicle from the back that would like to maintain its speed.
+    (;
+        initial_lane = -1.0,
+        initial_progress = -3.0,
+        initial_speed = 0.4,
+        target_speed = 0.4,
+        target_lane = -1.0,
+        speed_cost = 1.5,
+        prox_cost = 0.3,
+    ),
+    # Slow truck on the right lane
+    (;
+        initial_lane = 1.0,
+        initial_progress = 2,
+        initial_speed = 0.20,
+        target_speed = 0.20,
+        speed_cost = 1.0,
+        target_lane = 1.0,
+        prox_cost = 0.05,
+    ),
+    # Slow truck on the right lane
+    (;
+        initial_lane = 1.0,
+        initial_progress = 4,
+        initial_speed = 0.20,
+        target_speed = 0.20,
+        speed_cost = 1.0,
+        target_lane = 1.0,
+        prox_cost = 0.05,
+    ),
+    # Fast vehicle on the left lane wishing to merge back on the right lane and slow down
+    (;
+        initial_lane = -1.0,
+        initial_progress = 6,
+        initial_speed = 0.3,
+        target_speed = 0.2,
+        speed_cost = 1.5,
+        target_lane = 1.0,
+        prox_cost = 0.3,
+    ),
+]
+
 end
